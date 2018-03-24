fdda0516152721a42027bf136a4318671aaedacb
AmsterGet <amsterget@gmail.com>
2018-03-24 14:36:33 +0300

    Separated part of common css and fixed

--- common/slick/settings.js
+++ common/slick/settings.js
@@ -1,10 +1,92 @@
-function getSlides(selector) {
+function getElements(selector) {
     return document.querySelectorAll(selector) || [];
 }
 
 function initSlick() {
+    $('.complexes-slider').slick({
+        infinite: false,
+        speed: 1200,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        initialSlide: 2,
+        draggable: false,
+        swipe: false,
+        touchMove: false,
+        prevArrow: '.slider__btn-left',
+        nextArrow: '.slider__btn-right',
+        centerMode: true,
+        centerPadding: '0px',
+
+        responsive: [
+            {
+                breakpoint: 2490,
+                settings: {
+                    centerPadding: '260px',
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 2000,
+                settings: {
+                    centerPadding: '200px',
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 1780,
+                settings: {
+                    centerPadding: '140px',
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 1549,
+                settings: {
+                    centerPadding: '100px',
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 1450,
+                settings: {
+                    centerPadding: '60px',
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 1300,
+                settings: {
+                    centerPadding: '10px',
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 1270,
+                settings: {
+                    centerPadding: '0px',
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 1100,
+                settings: {
+                    centerPadding: '50px',
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            }
+        ]
+    });
+
     $('.slider__btn-left').click(function () {
-        var slides = getSlides('.slider-item-block');
+        var slides = getElements('.slider-item-block');
         if (slides[1].classList.contains('slick-current')) {
             this.classList.add('hide');
             console.log(this);
@@ -14,7 +96,7 @@ function initSlick() {
 
 
     $('.slider__btn-right').click(function () {
-        var slides = getSlides('.slider-item-block');
+        var slides = getElements('.slider-item-block');
         if (slides[slides.length - 2].classList.contains('slick-current')) {
             this.classList.add('hide');
             console.log(this);
@@ -39,7 +121,6 @@ function initSlick() {
                     centerPadding: '400px',
                     slidesToShow: 3,
                     slidesToScroll: 1,
-                    // arrows: false
                 }
             },
             {
@@ -48,7 +129,6 @@ function initSlick() {
                     centerPadding: '260px',
                     slidesToShow: 3,
                     slidesToScroll: 1,
-                    // arrows: false
                 }
             },
             {
@@ -57,7 +137,6 @@ function initSlick() {
                     centerPadding: '280px',
                     slidesToShow: 3,
                     slidesToScroll: 1,
-                    // arrows: false
                 }
             },
             {
@@ -66,7 +145,6 @@ function initSlick() {
                     centerPadding: '155px',
                     slidesToShow: 3,
                     slidesToScroll: 1,
-                    // arrows: false
                 }
             },
             {
@@ -75,7 +153,6 @@ function initSlick() {
                     centerPadding: '140px',
                     slidesToShow: 3,
                     slidesToScroll: 1,
-                    // arrows: false
                 }
             },
             {
@@ -84,25 +161,14 @@ function initSlick() {
                     centerPadding: '100px',
                     slidesToShow: 3,
                     slidesToScroll: 1,
-                    // arrows: false
                 }
             },
-            // {
-            //     breakpoint: 1100,
-            //     settings: {
-            //         centerPadding: '100px',
-            //         slidesToShow: 3,
-            //         slidesToScroll: 1,
-            //         // arrows: false
-            //     }
-            // },
             {
                 breakpoint: 1000,
                 settings: {
                     centerPadding: '88px',
                     slidesToShow: 3,
                     slidesToScroll: 1,
-                    // arrows: false
                 }
             }
         ]
@@ -119,11 +185,6 @@ function initSlick() {
         touchMove: false,
         prevArrow: '.forms-slider__btn-left',
         nextArrow: '.forms-slider__btn-right',
-        // swipeToSlide: true,
-        // focusOnSelect: true,
-        // centerMode: true,
-        // centerPadding: '280px',
-        // arrows: false,
         responsive: [
             {
                 breakpoint: 1620,
@@ -131,7 +192,6 @@ function initSlick() {
                     centerPadding: '200px',
                     slidesToShow: 1,
                     slidesToScroll: 1,
-                    // arrows: false
                 }
             },
             {
@@ -140,7 +200,6 @@ function initSlick() {
                     centerPadding: '110px',
                     slidesToShow: 1,
                     slidesToScroll: 1,
-                    // arrows: false
                 }
             }
         ]
@@ -167,115 +226,27 @@ initSlick();
       $('.popup').removeClass('activePopup');
   }
 
+  $(document).ready(function(){
+     $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+     $('.scrollup').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+
+  });
+
   function scrollToForm(elementClass) {
-      var slides = getSlides('.block-forms');
+      var slides = getElements('.block-forms');
       var slider = $('.forms-slider');
       if (elementClass==='complexes-slider-button') {
           slider.slick('slickGoTo',slides.length-1);
       }
       $('html, body').animate({scrollTop: slider.offset().top}, 700);
   }
-
-  // $('.complexes-slider-button').click(function () {
-  //
-  // });
-
-// $('.complexes-slider').slick({
-//     infinite: false,
-//     speed: 1200,
-//     slidesToShow: 5,
-//     slidesToScroll: 1,
-//     initialSlide: 2,
-//     draggable: false,
-//     swipe: false,
-//     touchMove: false,
-//     prevArrow: '.slider__btn-left',
-//     nextArrow: '.slider__btn-right',
-//     // swipeToSlide: true,
-//     // focusOnSelect: true,
-//     centerMode: true,
-//     centerPadding: '0px',
-//     // arrows: false,
-//
-//     responsive: [
-//         {
-//             breakpoint: 2490,
-//             settings: {
-//                 centerPadding: '260px',
-//                 slidesToShow: 3,
-//                 slidesToScroll: 1,
-//                 // arrows: false
-//             }
-//         },
-//         {
-//             breakpoint: 2000,
-//             settings: {
-//                 centerPadding: '200px',
-//                 slidesToShow: 3,
-//                 slidesToScroll: 1,
-//                 // arrows: false
-//             }
-//         },
-//         {
-//             breakpoint: 1780,
-//             settings: {
-//                 centerPadding: '140px',
-//                 slidesToShow: 3,
-//                 slidesToScroll: 1,
-//                 // arrows: false
-//             }
-//         },
-//         {
-//             breakpoint: 1549,
-//             settings: {
-//                 centerPadding: '100px',
-//                 slidesToShow: 3,
-//                 slidesToScroll: 1,
-//                 // arrows: false
-//             }
-//         },
-//         {
-//             breakpoint: 1450,
-//             settings: {
-//                 centerPadding: '60px',
-//                 slidesToShow: 3,
-//                 slidesToScroll: 1,
-//                 // arrows: false
-//             }
-//         },
-//         {
-//             breakpoint: 1300,
-//             settings: {
-//                 centerPadding: '10px',
-//                 slidesToShow: 3,
-//                 slidesToScroll: 1,
-//                 // arrows: false
-//             }
-//         },
-//         {
-//             breakpoint: 1270,
-//             settings: {
-//                 centerPadding: '0px',
-//                 slidesToShow: 3,
-//                 slidesToScroll: 1,
-//                 // arrows: false
-//             }
-//         },
-//         {
-//             breakpoint: 1100,
-//             settings: {
-//                 centerPadding: '50px',
-//                 slidesToShow: 3,
-//                 slidesToScroll: 3,
-//                 // arrows: false
-//             }
-//         }
-//     ]
-// });
-
-
-
-
-
-
-
